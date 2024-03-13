11b8ea6e63730d0c844b9859d13cd42844bff20d
ImDarkly <svit.darkly@gmail.com>
2024-03-13 13:51:55 +0200

    build: added basename to router

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Layout from '@/lib/layout';
 import Routings from '@/lib/router/Routings';
 
+const isProduction = process.env.NODE_ENV === 'production';
 const App = () => (
-  <Router>
+  <Router basename={isProduction ? '/skattjakt' : ''}>
     <Layout>
       <Routings />
     </Layout>
