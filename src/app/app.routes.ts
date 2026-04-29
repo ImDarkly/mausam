66734699a4ec3a258a44d4545020cae197e55156
ImDarkly <svit.darkly@gmail.com>
2026-04-29 23:42:30 +0300

    feat: scaffold project with routing and page stubs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SearchPage } from './pages/search-page/search-page';
+import { ResultPage } from './pages/result-page/result-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SearchPage },
+  { path: 'outfit/:city', component: ResultPage },
+];
